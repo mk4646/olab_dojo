b5ab224e542fe46ab20a64e921f9df95ba4841ed
mk4646 <83286959+mk4646@users.noreply.github.com>
2021-05-20 00:33:21 +0900

    Delete lv5 directory

--- lv5/clock.js
+++
@@ -1,5 +0,0 @@
-function clock(){
-    document.getElementById('date').innerText = now.toLocaleDateString();
-    document.getElementById('time').innerText = now.toLocaleTimeString();
-}
-setInterval(clock, 1000);
